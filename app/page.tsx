96bf935b3d23884b9051207d2bec101b2612af65
rh3t <keegan@rh3t.dev>
2024-12-03 00:41:23 -0500

    Commit v0.1.7

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,15 @@ const Index = () => {
             <Separator/>
 
             <p style = {{width: '50vw'}}>
-                This is an archive containing almost everything i've developed and worked on publically, including some other miscellaneous things to take a look at. Everything is served on a CloudFlare R2 bucket and you can check the status of the project respectively. <br/><br/> Everything is sorted by complexity.
+                This is an archive containing almost everything i've developed and worked on publically, including some other miscellaneous things to take a look at. Everything is served on a CloudFlare R2 bucket and you can check the status of the project respectively. <br/><br/> Everything is sorted by complexity (top to bottom).
             </p>
         
             <Separator/>
+
+            <Header id = 'prototype' text = 'Prototype (2021)' working = {true}/>
+            <Object cdn = {true} location = '/prototype/index.html'/>
+            <Object cdn = {true} location = '/prototype/game.sb3'/>
+            <br/>
 
             <Header id = 'thelastfew' text = 'The Last Few (2021)' working = {true}/>
             <Object cdn = {true} location = '/the-last-few/index.html'/>
